8676571eedadf9678b063131fad6a6fb876c9800
Tring-vignesh <vignesh.g@tringapps.com>
2025-03-12 17:04:21 +0530

    Protected route,my problems ,edit problem,delete problem,delete test cases added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,41 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route ,BrowserRouter} from "react-router-dom";
+import { ToastContainer } from "react-toastify";  
+import "react-toastify/dist/ReactToastify.css";
+
 import CodeEditor from "./components/CodeEditor";
 import UserDashboard from "./User/UserDashboard";
 import ProblemList from "./User/ProblemList";
-// import Profile from "./User/Profile"; 
-import "./App.css";
 import AddProblem from "./User/AddProblem";
 import WelcomePage from "./components/WelcomePage";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";  
+import MyProblems from "./User/Myproblems.jsx";
+import EditProblem from "./User/EditProblem.jsx";
 
 function App() {
   return (
-    <Routes>
-    <Route path="/" element={<WelcomePage />} />
-    <Route path="signup" element={<SignUp />} />
-    <Route path="signin" element={<SignIn />} />
-    <Route path="dashboard" element={<UserDashboard />}>
-      <Route path="problems" element={<ProblemList />} />
-      <Route path="add-problems" element={<AddProblem />} />
-    </Route>
-    <Route path="problems/:id" element={<CodeEditor />} />
-  </Routes>
+    <>
+    <BrowserRouter>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<UserDashboard />}>
+            <Route index element={<ProblemList />} />
+            <Route path="problems" element={<ProblemList />} />
+            <Route path="add-problems" element={<AddProblem />} />
+            <Route path="my-problems" element={<MyProblems />} />
+            <Route path="edit-problem/:id" element={<EditProblem />} />
+          </Route>
+          <Route path="problems/:id" element={<CodeEditor />} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-const client = new ApolloClient({
-  uri: "http://localhost:4000", 
-  cache: new InMemoryCache(),
-});
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import client from "../apolloClient.js";
+createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
-  <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+
+        <App />
+
+    </StrictMode>
   </ApolloProvider>
-  ,
-)
+);
