6a15a966e29f22c02970b80f3245baf47e0535fc
Tring-vignesh <vignesh.g@tringapps.com>
2025-03-12 09:40:44 +0530

    Improve Folder Structure and login,signup Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import CodeEditor from './components/CodeEditor';
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import CodeEditor from "./components/CodeEditor";
+import UserDashboard from "./User/UserDashboard";
+import ProblemList from "./User/ProblemList";
+// import Profile from "./User/Profile"; 
+import "./App.css";
+import AddProblem from "./User/AddProblem";
+import WelcomePage from "./components/WelcomePage";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 function App() {
- 
-
-  return <CodeEditor />;
+  return (
+    <Routes>
+    <Route path="/" element={<WelcomePage />} />
+    <Route path="signup" element={<SignUp />} />
+    <Route path="signin" element={<SignIn />} />
+    <Route path="dashboard" element={<UserDashboard />}>
+      <Route path="problems" element={<ProblemList />} />
+      <Route path="add-problems" element={<AddProblem />} />
+    </Route>
+    <Route path="problems/:id" element={<CodeEditor />} />
+  </Routes>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 const client = new ApolloClient({
   uri: "http://localhost:4000", 
   cache: new InMemoryCache(),
@@ -9,7 +10,9 @@ const client = new ApolloClient({
 createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>
   </ApolloProvider>
   ,
